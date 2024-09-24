cc5e842cf544c8c5b4268c66b3f5b2ace3a6512d
Leonardo Messa Wagner <leonardowagner22@outlook.com>
2024-09-23 21:39:21 -0300

    primeiro teste API

--- api/index.py
+++ api/index.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
+from coleta_e_grafico import coletar_e_gerar_grafico
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/coletar_e_grafico', methods=['GET'])
+def coletar_e_grafico_route():
+    gráfico_base64 = coletar_e_gerar_grafico()
+    return jsonify({'gráfico_base64': gráfico_base64})
 
-@app.route('/about')
-def about():
-    return 'About'
